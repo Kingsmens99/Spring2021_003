c12579c5e1b228ac0b1ad4df863dc324c7ec903b
Kingsmens99 <66391690+Kingsmens99@users.noreply.github.com>
2021-03-22 19:48:30 -0500

    Added my full name and travel destination

---
+++ TravelDestination.java
@@ -0,0 +1,5 @@
+public class TravelDestination{
+	public static void main(String[] args){
+		System.out.println("My name is Jonathan Sarazen. I wish to travel to Japan once this Epidemic goes away!");
+	}
+}
