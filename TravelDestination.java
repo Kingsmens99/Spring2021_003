f35a9e12c287a06771e0004fcc07205c45c094c9
Kingsmens99 <66391690+Kingsmens99@users.noreply.github.com>
2021-03-22 19:06:50 -0500

    Add new file called TravelDestination that will print out my travel plans!

---
+++ TravelDestination.java
@@ -0,0 +1,5 @@
+public class TravelDestination{
+	public static void main(String[] args){
+		System.out.println("My name is Jonathan Sarazen and I cannot wait to travel to Japan after this epidemic!");
+	}
+}
